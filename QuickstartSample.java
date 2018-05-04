8ae0af18b2cb3fe41e9b27b0fa6060775193b407
hihi5345 <hihi5345@naver.com>
2018-05-04 19:30:16 +0900

    STTandNaturalLanguage

---
+++ QuickstartSample.java
@@ -0,0 +1,152 @@
+// Imports the Google Cloud client library
+import com.google.cloud.language.v1.*;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.speech.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class QuickstartSample {
+
+    private static String text = "";
+
+    public static Sentiment analyzingSentiment(String text) throws Exception{
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+            AnalyzeSentimentResponse response = language.analyzeSentiment(doc);
+            Sentiment sentiment = response.getDocumentSentiment();
+            if (sentiment == null) {
+                System.out.println("No sentiment found");
+            } else {
+                System.out.printf("Sentiment magnitude: %.3f\n", sentiment.getMagnitude());
+                System.out.printf("Sentiment score: %.3f\n", sentiment.getScore());
+            }
+            return sentiment;
+        }
+    }
+
+    public static void analyzingEntity(String text) throws Exception{
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+            AnalyzeEntitiesRequest request = AnalyzeEntitiesRequest.newBuilder()
+                    .setDocument(doc)
+                    .setEncodingType(EncodingType.UTF16)
+                    .build();
+
+            AnalyzeEntitiesResponse response = language.analyzeEntities(request);
+
+            // Print the response
+            for (Entity entity : response.getEntitiesList()) {
+                System.out.printf("Entity: %s\n", entity.getName());
+                System.out.printf("Salience: %.3f\n", entity.getSalience());
+                System.out.println("Metadata: ");
+                for (Map.Entry<String, String> entry : entity.getMetadataMap().entrySet()) {
+                    System.out.printf("%s : %s", entry.getKey(), entry.getValue());
+                }
+                for (EntityMention mention : entity.getMentionsList()) {
+                    System.out.printf("Begin offset: %d\n", mention.getText().getBeginOffset());
+                    System.out.printf("Content: %s\n", mention.getText().getContent());
+                    System.out.printf("Type: %s\n\n", mention.getType());
+                }
+            }
+        }
+    }
+
+
+    public static List<Token> analyzingSyntax(String text) throws Exception{
+        // Instantiate the Language client com.google.cloud.language.v1.LanguageServiceClient
+        try (LanguageServiceClient language = LanguageServiceClient.create()) {
+            Document doc = Document.newBuilder()
+                    .setContent(text)
+                    .setType(Type.PLAIN_TEXT)
+                    .build();
+            AnalyzeSyntaxRequest request = AnalyzeSyntaxRequest.newBuilder()
+                    .setDocument(doc)
+                    .setEncodingType(EncodingType.UTF16)
+                    .build();
+            // analyze the syntax in the given text
+            AnalyzeSyntaxResponse response = language.analyzeSyntax(request);
+            // print the response
+            for (Token token : response.getTokensList()) {
+                System.out.printf("\tText: %s\n", token.getText().getContent());
+                System.out.printf("\tBeginOffset: %d\n", token.getText().getBeginOffset());
+                System.out.printf("Lemma: %s\n", token.getLemma());
+                System.out.printf("PartOfSpeechTag: %s\n", token.getPartOfSpeech().getTag());
+                System.out.printf("\tAspect: %s\n", token.getPartOfSpeech().getAspect());
+                System.out.printf("\tCase: %s\n", token.getPartOfSpeech().getCase());
+                System.out.printf("\tForm: %s\n", token.getPartOfSpeech().getForm());
+                System.out.printf("\tGender: %s\n", token.getPartOfSpeech().getGender());
+                System.out.printf("\tMood: %s\n", token.getPartOfSpeech().getMood());
+                System.out.printf("\tNumber: %s\n", token.getPartOfSpeech().getNumber());
+                System.out.printf("\tPerson: %s\n", token.getPartOfSpeech().getPerson());
+                System.out.printf("\tProper: %s\n", token.getPartOfSpeech().getProper());
+                System.out.printf("\tReciprocity: %s\n", token.getPartOfSpeech().getReciprocity());
+                System.out.printf("\tTense: %s\n", token.getPartOfSpeech().getTense());
+                System.out.printf("\tVoice: %s\n", token.getPartOfSpeech().getVoice());
+                System.out.println("DependencyEdge");
+                System.out.printf("\tHeadTokenIndex: %d\n", token.getDependencyEdge().getHeadTokenIndex());
+                System.out.printf("\tLabel: %s\n\n", token.getDependencyEdge().getLabel());
+
+            }
+
+
+
+            return response.getTokensList();
+        }
+    }
+
+    public static void main(String... args) throws Exception {
+
+        try (SpeechClient speechClient = SpeechClient.create()) {
+
+            // The path to the audio file to transcribe
+            String fileName = "resources/jungseok_sleepy.wav";
+
+            // Reads the audio file into memory
+            Path path = Paths.get(fileName);
+            byte[] data = Files.readAllBytes(path);
+            ByteString audioBytes = ByteString.copyFrom(data);
+
+            // Builds the sync recognize request
+            RecognitionConfig config = RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(16000)
+                    .setLanguageCode("ko-KR")
+                    .build();
+
+            RecognitionAudio audio = RecognitionAudio.newBuilder()
+                    .setContent(audioBytes)
+                    .build();
+
+            // Performs speech recognition on the audio file
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
+
+            for (SpeechRecognitionResult result : results) {
+                // There can be several alternative transcripts for a given chunk of speech. Just use the
+                // first (most likely) one here.
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                System.out.printf("Transcription: %s%n", alternative.getTranscript());
+                text = alternative.getTranscript();
+            }
+        }
+
+        System.out.println("Sentiment Anayzing.");
+        analyzingSentiment(text);
+        System.out.println("Entity Anayzing.");
+        analyzingEntity(text);
+        System.out.println("Syntax Anayzing.");
+        analyzingSyntax(text);
+
+    }
+}
